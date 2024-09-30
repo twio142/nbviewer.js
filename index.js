4d2c250d639941d24da9b08d9dd9b2ddc4bbeef5
twio142 <64556708+twio142@users.noreply.github.com>
2024-09-30 15:19:58 +0200

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+'use strict';
+
+/* Generate HTML from Jupyter Notebook */
+// https://github.com/tuxu/ipynb-quicklook/
+
+const fs = require("fs");
+
+function previewLocal(nbFile, htmlFile) {
+  const marked = require("marked");
+  const katex = require("katex");
+  const prism = require("prismjs");
+  const { JSDOM } = require("jsdom");
+  const { exception } = require("console");
+  const document = (new JSDOM("")).window.document;
+  const nbv = require("./nbv").nbv_constructor(document, {marked, prism, katex});
+  const target = document.createElement("div");
+  const nb = JSON.parse(fs.readFileSync(nbFile));
+  nbv.render(nb, target);
+  fs.writeFileSync(htmlFile, target.innerHTML);
+  return htmlFile;
+}
+
+function previewOnline(nbFile, htmlFile) {
+  const nb = JSON.parse(fs.readFileSync(nbFile));
+  const nbv = fs.readFileSync("nbv.js").toString();
+  const tmpl = fs.readFileSync("template.html").toString();
+  const uglify = require("uglify-js");
+  let html = tmpl
+    .replace("%nbv%", uglify.minify(nbv).code)
+    .replace("%nb-json%", JSON.stringify(nb));
+  fs.writeFileSync(htmlFile, html);
+  return htmlFile;
+}
+
+((nbFile, htmlFile) => {
+  htmlFile = htmlFile || nbFile.replace(/\.ipynb$/, ".html");
+  (process.env.USE_LOCAL ? previewLocal : previewOnline)(nbFile, htmlFile);
+})(process.argv[2], process.argv[3]);
