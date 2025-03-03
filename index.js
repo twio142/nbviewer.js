b88031081323b06b4bae18cbca7bc84a1d57f7f8
twio142 <64556708+twio142@users.noreply.github.com>
2025-03-03 05:00:07 +0100

    style: format code

--- index.js
+++ index.js
@@ -4,18 +4,18 @@
 /* Generate HTML from Jupyter Notebook */
 // https://github.com/tuxu/ipynb-quicklook/
 
-const fs = require("fs");
-const {resolve} = require("path");
+const fs = require('fs');
+const { resolve } = require('path');
 
 function previewLocal(nbFile, htmlFile) {
-  const marked = require("marked");
-  const katex = require("katex");
-  const prism = require("prismjs");
-  const { JSDOM } = require("jsdom");
+  const { JSDOM } = require('jsdom');
+  const katex = require('katex');
+  const marked = require('marked');
+  const prism = require('prismjs');
   // const { exception } = require("console");
-  const document = (new JSDOM("")).window.document;
-  const nbv = require("./nbv").nbv_constructor(document, {marked, prism, katex});
-  const target = document.createElement("div");
+  const document = (new JSDOM('')).window.document;
+  const nbv = require('./nbv').nbv_constructor(document, { marked, prism, katex });
+  const target = document.createElement('div');
   const nb = JSON.parse(fs.readFileSync(nbFile));
   nbv.render(nb, target);
   fs.writeFileSync(htmlFile, target.innerHTML);
@@ -24,17 +24,17 @@ function previewLocal(nbFile, htmlFile) {
 
 function previewOnline(nbFile, htmlFile) {
   const nb = JSON.parse(fs.readFileSync(nbFile));
-  const nbv = fs.readFileSync(resolve(__dirname, "nbv.js")).toString();
-  const tmpl = fs.readFileSync(resolve(__dirname, "template.html")).toString();
-  const uglify = require("uglify-js");
-  let html = tmpl
-    .replace("%nbv%", uglify.minify(nbv).code)
-    .replace("%nb-json%", JSON.stringify(nb));
+  const nbv = fs.readFileSync(resolve(__dirname, 'nbv.js')).toString();
+  const tmpl = fs.readFileSync(resolve(__dirname, 'template.html')).toString();
+  const uglify = require('uglify-js');
+  const html = tmpl
+    .replace('%nbv%', uglify.minify(nbv).code)
+    .replace('%nb-json%', JSON.stringify(nb));
   fs.writeFileSync(htmlFile, html);
   return htmlFile;
 }
 
 ((nbFile, htmlFile) => {
-  htmlFile = htmlFile || nbFile.replace(/\.ipynb$/, ".html");
+  htmlFile = htmlFile || nbFile.replace(/\.ipynb$/, '.html');
   (process.env.USE_LOCAL ? previewLocal : previewOnline)(nbFile, htmlFile);
 })(process.argv[2], process.argv[3]);
